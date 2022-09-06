4530bf1b5b1324f67768012f69b47437ed628006
juanchi425 <juanchimart229@gmail.com>
2022-09-06 19:36:31 -0300

    initial commit

---
+++ src/main/java/com/alkemy/icons/icons/IconsPaisApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.icons.icons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IconsPaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IconsPaisApplication.class, args);
+	}
+
+}
